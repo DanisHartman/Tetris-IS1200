3e8a0eadbbe53dd25f7c3ad855a05932f89c11eb
Daniel <daniel.hartman31@gmail.com>
2022-02-11 20:03:05 +0100

    Test

---
+++ test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int matrix[4][4] = {
+						{0,0,0,0},
+						{0,0,0,0},
+						{1,1,1,0},
+						{0,1,0,0}
+					};    
+	
+int i=0,j=0;    
+    
+void draw(int matrix [4][4], int offsetX, int offsetY) {
+	
+}
+   
+	
+	
+	
+	
+	
+  	
+	
+void main(void) {
+	draw(matrix, 2,3);
+}
